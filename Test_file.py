17e137e80f4cce0081e10706fb50e8ea5f4756b3
Khaled Sousi <khaledwns18@gmail.com>
2023-08-01 17:44:32 +0300

    Task1_final

--- Test_file.py
+++ Test_file.py
@@ -1,14 +1,38 @@
-name = input("Enter Your Name: ")
-# print(name)
-age = input("Enter Your Age: ")
-# print(age)
-date = input("Enter Your date: ")
-# print(date)
+# name = input("Enter Your Name: ")
+# # print(name)
+# age = input("Enter Your Age: ")
+# # print(age)
+# date = input("Enter Your date: ")
+# # print(date)
+#
+# text = f"""My name is {name}
+# my age is {age}
+# my birhday date is {date} """
+#
+# print(text)
+#
+# print(len(name))
 
-text = f"""My name is {name} 
-my age is {age} 
-my birhday date is {date} """
+a = "Welcome to my python file "
+b= len(a)
+print(b)
+print("o" in a)
+if "W" in a:
+    print("Yes Its in")
 
-print(text)
+print("w" not in a)
+# Slicing
+print(a[2:11])
+print(a[:9])
+print(a[6:])
 
+# capital and small letter
+print(a.upper())
+print(a.lower())
 
+# Strip for remove white space from beginning and ending
+print(a.strip())
+print(a.replace("W", "w"))
+
+# The split() method returns a list
+print(a.split(" "))
