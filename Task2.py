1571bfb3fedee9ea5cc2a7f569fb59c4d003a26b
Khaled Sousi <khaledwns18@gmail.com>
2023-08-01 20:24:24 +0300

    task2

---
+++ Task2.py
@@ -0,0 +1,10 @@
+user_name = input("Enter Your Name: ")
+mobile_number = input("Enter Your Mobile Number: ")
+year_of_birth = input("Enter Your Year Of Birth: ")
+mobile_number = mobile_number[0:3]+ "-" + mobile_number[3:7] + "-" + mobile_number[7:]
+current_year = 2023
+user_age = current_year - int(year_of_birth)
+mobile_number = mobile_number.split("-")
+print(user_name)
+print(user_age)
+print(mobile_number)
