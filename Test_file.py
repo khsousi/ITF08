c12e16e20735e9c3738d5b0a901520e5c8856e5c
Khaled Sousi <khaledwns18@gmail.com>
2023-08-02 16:14:54 +0300

    functions

--- Test_file.py
+++ Test_file.py
@@ -1,39 +1,46 @@
-# name = input("Enter Your Name: ")
-# # print(name)
-# age = input("Enter Your Age: ")
-# # print(age)
-# date = input("Enter Your date: ")
-# # print(date)
+# # name = input("Enter Your Name: ")
+# # # print(name)
+# # age = input("Enter Your Age: ")
+# # # print(age)
+# # date = input("Enter Your date: ")
+# # # print(date)
+# #
+# # text = f"""My name is {name}
+# # my age is {age}
+# # my birthday date is {date} """
+# #
+# # print(text)
+# #
+# # print(len(name))
 #
-# text = f"""My name is {name}
-# my age is {age}
-# my birhday date is {date} """
+# a = "Welcome to my python file "
+# b = len(a)
+# print(b)
+# print("o" in a)
+# if "W" in a:
+#     print("Yes Its in")
 #
-# print(text)
+# print("w" not in a)
+# # Slicing
+# print(a[2:11])
+# print(a[:9])
+# print(a[6:])
 #
-# print(len(name))
+# # capital and small letter
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+#
+# # Strip for remove white space from beginning and ending
+# print(a.strip())
+# print(a.replace("W", "w"))
+#
+# # The split() method returns a list
+# print(a.split(" "))
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+txt2 = f"My name is john, and I am {age}"
+print(txt2)
 
-a = "Welcome to my python file "
-b= len(a)
-print(b)
-print("o" in a)
-if "W" in a:
-    print("Yes Its in")
-
-print("w" not in a)
-# Slicing
-print(a[2:11])
-print(a[:9])
-print(a[6:])
-
-# capital and small letter
-print(a.upper())
-print(a.lower())
-
-# Strip for remove white space from beginning and ending
-print(a.strip())
-print(a.replace("W", "w"))
-
-# The split() method returns a list
-print(a.split(" "))
-
+print(10 > 11)
---
+++ funcions.py
@@ -0,0 +1,27 @@
+def sum_num(num1, num2):
+    summ = num1+num2
+    print(summ)
+    return summ
+
+sum_num(5,3)
+
+def sub_num(num1 , num2):
+    subb = num1-num2
+    print(subb)
+    return subb
+
+sub_num(5,3)
+
+def multibale_num(num1, num2):
+    multibale = num1*num2
+    print(multibale)
+    return multibale
+
+multibale_num(5,3)
+
+def devision_num(num1, num2):
+    devision = num1/num2
+    print(devision)
+    return devision
+
+devision_num(5,3)
