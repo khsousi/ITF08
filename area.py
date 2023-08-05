daf38940a62cf172bf66537df6b86987ba99b214
Khaled Sousi <khaledwns18@gmail.com>
2023-08-05 11:47:37 +0300

    area

--- area.py
+++ area.py
@@ -1,8 +1,8 @@
 def tringle():
     height = int(input("Enter Num1: "))
     base = int(input("Enter Num2: "))
-    tringle_area = (0.5*base)*height
-    return tringle_area
+    area = (0.5*base)*height
+    return area
 
 
 print(tringle())
@@ -10,8 +10,8 @@ print(tringle())
 
 def circle():
     radius = int(input("Enter radius: "))
-    circle_area = 3.14 * radius**2
-    return circle_area
+    area = 3.14 * radius** 2
+    return area
 
 
 print(circle())
@@ -20,8 +20,8 @@ print(circle())
 def rectangle():
     length = int(input("Enter length: "))
     width = int(input("Enter width: "))
-    area_rectangle= length * width
-    return area_rectangle
+    area = length * width
+    return area
 
 
 print(rectangle())
