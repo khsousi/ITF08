2d303bf35fa45d8cd4a8099222d6d32fa3cb71b0
Khaled Sousi <khaledwns18@gmail.com>
2023-08-19 19:31:00 +0300

    Loops

--- Loops.py
+++ Loops.py
@@ -1,14 +1,15 @@
 total = 0
 my_marks = []
+student_count = int(input("Enter Student Count: "))
 marks_count = int(input("Enter Marks Count: "))
+for j in range(0, student_count):
+    for i in range(0, marks_count):
+        mark = float(input(f"Enter mark {i+1}: "))
+        my_marks.append(mark)
 
-for i in range(0, marks_count):
-    mark = float(input(f"Enter mark {i+1}: "))
-    my_marks.append(mark)
+    average = sum(my_marks) / len(my_marks)
+    print(average)
+    print(my_marks)
 
-average = sum(my_marks) / len(my_marks)
-print(average)
-print(my_marks)
-
-print(f"Max Mark = {max(my_marks)}")
-print(f"Min Mark = {min(my_marks)}")
+    print(f"Max Mark = {max(my_marks)}")
+    print(f"Min Mark = {min(my_marks)}")
