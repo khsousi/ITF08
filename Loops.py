a79e2b686a48156e3ae8463a7b6ffa24bb897edf
Khaled Sousi <khaledwns18@gmail.com>
2023-08-19 09:55:13 +0300

    Loops

--- Loops.py
+++ Loops.py
@@ -1,1 +1,14 @@
+total = 0
+my_marks = []
+marks_count = int(input("Enter Marks Count: "))
 
+for i in range(0, marks_count):
+    mark = float(input(f"Enter mark {i+1}: "))
+    my_marks.append(mark)
+
+average = sum(my_marks) / len(my_marks)
+print(average)
+print(my_marks)
+
+print(f"Max Mark = {max(my_marks)}")
+print(f"Min Mark = {min(my_marks)}")
